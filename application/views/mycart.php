15ce17bb5eaeb2a5b22e2060660930f16be8a180
PrimeroBestia <primebeast@ymail.com>
2018-03-02 14:57:25 +0800

    File arrangement fix

---
+++ application/views/mycart.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FOOD CORNER</title>
+	<link rel="stylesheet" type="text/css" href="assets/User/css/user.css">
+	 <link href="https://fonts.googleapis.com/css?family=Spectral:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+</head>
+<body>
+	<header>
+		<div class="row">
+			<div class="logo">
+				<img src="assets/User/img/logo.png">
+			</div>
+			<ul class="nav-main">
+				<li><a href="">HOME</a></li>
+				<li><a href="">ORDER NOW</a></li>
+				<li><a href="">RECIPE BOOK</a></li>
+				<li><a href="">ABOUT US</a></li>
+				<li><a href="" class="nav-item">USER</a>
+					<div class="nav-content">
+						<div class="nav-sub">
+							<ul>
+                <li><a href="myaccount.php">My Account</a></li>
+                <li><a href="mypurchase.php">My Purchase</a></li>
+                <li><a href="mycart.php">My Cart</a></li>
+                <li><a href="">Log Out</a></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</header>
+
+	<div class="banner">
+			<div class="side">
+				<h3 class="displayname">User's Name</h3>
+				<ul class="sidemenu">
+					<li><a href="myaccount.php">My Account</a>
+							<ul>
+								<li><a href="profile.php">Profile </a></li>
+								<li><a href="bankcards.php">Bank & Cards</a></li>
+								<li><a href="addresses.php">Addresses</a></li>
+								<li><a href="changepw.php">Change Password</a></li>
+							</ul>
+					</li>
+					<li><a href="mp-topay.php">My Purchase</a></li>
+					<li><a href="">My Cart</a></li>
+				</ul>
+			</div>
+
+
+			<div class="content">
+				<h4 class="msc">Shopping Cart</h4>
+				 	<div class="page">
+     				 	<table class="cart">
+       					 	<thead>
+       						   <tr>
+    								<th class="first">Product</th>
+  						          	<th class="second">Unit Price</th>
+   						         	<th class="third">Bundle</th>
+  						         	<th class="fourth">Total Price</th>
+            						<th class="fifth">Actions</th>
+          						</tr>
+      		  				</thead>
+
+      		  				<tbody class="table-body">
+  								<tr class="productitm">
+   									<td><img src="img/cook_lunch.png" class="sample-product"></td>
+    								<td>₱ 200.00</td>
+    								<td><input type="number" value="1" min="0" max="99" class="qtyinput"></td>
+    								<td>₱ 200.00</td>
+   									<td><a href="">Remove</a></td>
+  								</tr>
+  								<tr class="productitm">
+   									<td><img src="img/cook_breakfast.png" class="sample-product"></td>
+    								<td>₱ 200.00</td>
+    								<td><input type="number" value="1" min="0" max="99" class="qtyinput"></td>
+    								<td>₱ 200.00</td>
+   									<td><a href="">Remove</a></td>
+  								</tr>
+  								<tr class="productitm">
+   									<td><img src="img/cook_dinner.png" class="sample-product"></td>
+    								<td>₱ 200.00</td>
+    								<td><input type="number" value="1" min="0" max="99" class="qtyinput"></td>
+    								<td>₱ 200.00</td>
+   									<td><a href="">Remove</a></td>
+  								</tr>
+  								<tr class="productitm">
+   									<td><img src="img/cook_dessert.png" class="sample-product"></td>
+    								<td>₱ 200.00</td>
+    								<td><input type="number" value="1" min="0" max="99" class="qtyinput"></td>
+    								<td>₱ 200.00</td>
+   									<td><a href="">Remove</a></td>
+  								</tr>
+                  <!--tr class="extracosts">
+    								<td>Shipping & Tax</td>
+    								<td></td>
+    								<td>₱ 200.00</td>
+    								<td></td>
+ 								</tr>
+
+  								<tr class="totalprice">
+    								<td>Total:</td>
+    								<td>&nbsp;</td>
+    								<td>₱ 255.45</td>
+  								</tr>
+
+								<tr class="checkoutrow">
+    								<td class="checkout"><button class="submitbtn">Checkout Now!</button></td>
+  								</tr>-->
+							</tbody>
+						</table>
+					</div>
+			</div>
+	</div>
+
+	<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <p>Copyright &copy; 2018 Food Corner</p>
+                </div>
+                <div class="col-md-4">
+                    <ul class="social-icons">
+                        <li><a rel="nofollow" href="http://www.facebook.com/templatemo" target="_parent"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
