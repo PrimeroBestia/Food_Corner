d53ad4c803fb889105e5ad907762e7a1346112b7
PrimeroBestia <primebeast@ymail.com>
2018-03-09 19:12:26 +0800

    Nilabas sa .zip

---
+++ application/views/Recipe2/Recipe Book.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+    <head>
+        <title>Recipes</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style_common.css" />
+        <link rel="stylesheet" type="text/css" href="css/style5.css" />
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css' />
+    </head>
+            <h1>Recipes</span></h1>
+            <div class="main">
+                <div class="view view-fifth">
+                    <img src="images/1.jpg" />
+                    <div class="mask">
+                        <h2>Western Dishes</h2>
+                        <p>Find the best, easy fast meal ideas from Western food recipes. </p>
+                        <a href="#" class="info">View Dishes</a>
+                    </div>
+                </div>
+                <div class="view view-fifth">
+                    <img src="images/5.jpg" />
+                    <div class="mask">
+                        <h2>Eastern Dishes</h2>
+                        <p>Find the best, easy fast meal ideas from Eastern food recipes. </p>
+                        <a href="#" class="info">View Dishes</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+		
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
---
+++ application/views/Recipe2/Western.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en" class="demo-3 no-js">
+	<head>
+		<title> Western Dishes </title>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+	
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo2.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<script src="js/snap.svg-min.js"></script>
+	</head>
+	<body>
+	<h1 text align = "center">Western Dishes</h1>
+			<section id="grid" class="grid clearfix">
+				<a href="#" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+					<figure>
+						<img src="img/1.jpg" />
+						<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+						<figcaption>
+							<h2>Cheesy Ranch Tuna</h2>
+							<button>View Recipe</button>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+					<figure>
+						<img src="img/2.jpg" />
+						<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+						<figcaption>
+							<h2>Easy Chicken Marinara</h2>
+							<button>View Recipe</button>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+					<figure>
+						<img src="img/3.jpg" />
+						<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+						<figcaption>
+							<h2>Lasagne</h2>
+							<button>View Recipe</button>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+					<figure>
+						<img src="img/4.jpg" />
+						<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+						<figcaption>
+							<h2>Salmon Burger</h2>
+							<button>View Recipe</button>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+					<figure>
+						<img src="img/5.jpg" />
+						<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+						<figcaption>
+							<h2>Spicy Sausage and Spaghetti</h2>
+							<button>View Recipe</button>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+					<figure>
+						<img src="img/6.jpg" />
+						<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+						<figcaption>
+							<h2>Sloppy Joes</h2>
+							<button>View Recipe</button>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+					<figure>
+						<img src="img/7.jpg" />
+						<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+						<figcaption>
+							<h2>Turkey Pot Pie</h2>
+							<button>View Recipe</button>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+					<figure>
+						<img src="img/8.jpg" />
+						<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+						<figcaption>
+							<h2>Chicken Curry</h2>
+							<button>View Recipe</button>
+						</figcaption>
+					</figure>
+				</a>
+			</section>
+			
+		</div><!-- /container -->
+		<script>
+			(function() {
+	
+				function init() {
+					var speed = 300,
+						easing = mina.backout;
+
+					[].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+						var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+							pathConfig = {
+								from : path.attr( 'd' ),
+								to : el.getAttribute( 'data-path-hover' )
+							};
+
+						el.addEventListener( 'mouseenter', function() {
+							path.animate( { 'path' : pathConfig.to }, speed, easing );
+						} );
+
+						el.addEventListener( 'mouseleave', function() {
+							path.animate( { 'path' : pathConfig.from }, speed, easing );
+						} );
+					} );
+				}
+
+				init();
+
+			})();
+		</script>
+	</body>
+</html>
