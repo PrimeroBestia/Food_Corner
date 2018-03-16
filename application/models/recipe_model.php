c2893959ac9e9b065e85b5a801a6e85d77a07c56
PrimeroBestia <primebeast@ymail.com>
2018-03-16 23:48:38 +0800

    Recipe Backend Done

--- application/models/recipe_model.php
+++ application/models/recipe_model.php
@@ -50,4 +50,12 @@ class recipe_model extends CI_Model {
 		$result = $this->db->get('recipe');
 		return $result->row_array();
     }
+    public function get_ingridients($ingid){
+    	$this->db->select('*');
+    	$this->db->where('ingridients_list.ing_list_id',$ingid);    
+		$this->db->from('ingridients_list');
+		$this->db->join('ingredients', 'ingredients.ing_id = ingridients_list.ing_id');
+    	$result = $this->db->get();
+		return $result->result_array();
+    }
 }
