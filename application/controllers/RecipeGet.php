bcc88e1b99a3d4c5a2556b57b001315770146c41
PrimeroBestia <primebeast@ymail.com>
2018-03-17 00:14:39 +0800

    RecipePage Logic Done

--- application/controllers/RecipeGet.php
+++ application/controllers/RecipeGet.php
@@ -10,12 +10,20 @@ class RecipeGet extends CI_Controller {
 	public function get_recipe($r_id){
 		$recipe = $this->recipe_model->get_recipe($r_id);
 		$data['recipe'] = $recipe;
-		print_r($recipe);
 		$inglist = $recipe['ing_list_id'];
 		$ingridients = $this->recipe_model->get_ingridients($inglist);
-		print_r($ingridients);
 		$data['recipe'] = $recipe;
-		$data['ingridients'] =$ingridients;
+		$data['ingridients'] =$ingridients;	
+		if (isset($_SESSION['username'])) {
+			$this->load->view('template/headerlogin');
+			$this->load->view('Recipe2/RecipePage',$data);
+			$this->load->view('template/footer');
+		}
+		else{
+			$this->load->view('template/header');
+			$this->load->view('Recipe2/RecipePage',$data);
+			$this->load->view('template/footer');
+		}	
 	}
 }
 ?>
