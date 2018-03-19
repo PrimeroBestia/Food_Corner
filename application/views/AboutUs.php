c722a756b49f45fda20f77fcf806c0cd5594b917
AdoAlvarez56 <adoalvarez56@gmail.com>
2018-03-19 09:53:20 +0800

    Initial commit

---
+++ application/views/AboutUs.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <title>Food Corner - About Us</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="assets/FoodC/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/FoodC/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="assets/FoodC/css/fontAwesome.css">
+        <link rel="stylesheet" href="assets/FoodC/css/hero-slider.css">
+        <link rel="stylesheet" href="assets/FoodC/css/owl-carousel.css">
+        <link rel="stylesheet" href="assets/FoodC/css/templatemo-style.css">
+
+        <link href="<?php echo base_url()."https://fonts.googleapis.com/css?family=Spectral:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i"?>" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+    </head>
+
+<body>
+    <div class="header">
+            <nav class="navbar navbar-inverse" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div id="main-nav" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="Welcome">Home</a></li>
+                        <li><a href="">Order Now</a></li>
+                        <li><a href="RecipeBView">Recipe Book</a></li>
+                        <li><a href="AboutUsView">About Us</a></li>
+                        <li><a href="LoginView">Sign up</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+
+    <section class="page-heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>About Us</h1>
+                    <p>The website is to be created not only for those who want a fast way of getting fresh ingredients, but also for those who are willing to learn cooking different cuisines.<p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="aboutus-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="aboutus-item">
+                        <img src="img/bg1L.jpg" alt="">
+                        <div class="date">13 Jan 2018</div>
+                        <div class="down-content">
+                            <h4>Food Corner</h4>
+                            <span>Branding / Admin</span>
+                            <p>FOOD CORNER is a website designed to innovate the food delivery industry. This website allows registered customers/users to choose a recipe of they desire to learn from a wide variety of cuisines originating from different regions of the world. The website also offers aid in learning these recipes by giving the customer the ability to purchase a package of ingredients corresponding to the recipe of their choosing right to their doorstep, each ingredient being measured, fresh, and ready to cook.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="sign-up">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="heading">
+                        <h2>Signup for our newsletters</h2>
+                    </div>
+                </div>
+            </div>
+            <form id="contact" action="" method="get">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-3">
+                        <fieldset>
+                            <input name="email" type="text" class="form-control" id="email" placeholder="Enter your email here..." required="">
+                        </fieldset>
+                    </div>
+                    <div class="col-md-2">
+                        <fieldset>
+                            <button type="submit" id="form-submit" class="btn">Send Message</button>
+                        </fieldset>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <p>Copyright &copy; 2018 Food Corner</p>
+                </div>
+                <div class="col-md-4">
+                    <ul class="social-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
