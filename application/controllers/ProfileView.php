aaa660496ee0f0577719bfdb0348b8706ce60eb9
allineate <genesys0880@gmail.com>
2018-03-20 18:33:59 +0800

    userprofile-update

--- application/controllers/ProfileView.php
+++ application/controllers/ProfileView.php
@@ -17,4 +17,149 @@ class ProfileView extends CI_Controller {
 			redirect(base_url());
 		}
 	}
+	public function addresses()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/addresses');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function bankcards()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/bankcards');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function cart()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/cart');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function changepw()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/changepw');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function mp_cancelled()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/mp_cancelled');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function mp_completed()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/mp_completed');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function mp_topay()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/mp_topay');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function mp_torecieved()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/mp_torecieved');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function mp_toship()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/mp_toship');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function myaccount()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/myaccount');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function mycart()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/mycart');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function mypurchase()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/mypurchase');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+	public function profile()
+	{
+		if(isset($_SESSION['email'])){
+			$this->load->view('template/headerlogin');
+			$this->load->view('UserProfile/profile');
+			$this->load->view('template/footer');
+		}
+		else{
+			redirect(base_url());
+		}
+	}
+
+
 }
