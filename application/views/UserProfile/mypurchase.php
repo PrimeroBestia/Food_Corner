d41bdb9a32f8d777a018e5052b46b1e04e486dbc
Rose Anne Cambel <32568285+araaangg@users.noreply.github.com>
2018-03-21 23:04:43 +0800

    cart removed and css fix

--- application/views/UserProfile/mypurchase.php
+++ application/views/UserProfile/mypurchase.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>FOOD CORNER</title>
-	<link rel="stylesheet" type="text/css" href="css/user.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/UserProfile/user.css'); ?>" />
 	 <link href="https://fonts.googleapis.com/css?family=Spectral:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
      <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
@@ -13,7 +13,7 @@
 
 	<div class="banner">
       <div class="side">
-        <h3 class="displayname">User's Name</h3>
+        <h3 class="displayname">username</h3>
         <ul class="sidemenu">
           <li><a href="<?php echo site_url('/ProfileView/myaccount');?>">My Account</a>
               <ul>
@@ -24,7 +24,7 @@
               </ul>
             </li>
           <li><a href="<?php echo site_url('/ProfileView/mypurchase');?>">My Purchase</a></li>
-          <li><a href="<?php echo site_url('/ProfileView/mycart');?>">My Cart</a></li>
+
         </ul>
       </div>
 			<div class="up-content">
