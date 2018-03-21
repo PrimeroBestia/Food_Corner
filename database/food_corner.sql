60445684a302130abc20ad9425a5066226be7d41
PrimeroBestia <primebeast@ymail.com>
2018-03-22 05:16:08 +0800

    Update Database

--- database/food_corner.sql
+++ database/food_corner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2018 at 11:56 AM
+-- Generation Time: Mar 21, 2018 at 10:15 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -159,15 +159,16 @@ INSERT INTO `ingridients_list` (`ing_list_id`, `ing_id`, `amount`) VALUES
 CREATE TABLE `my_cart` (
   `c_id` varchar(255) NOT NULL,
   `r_id` bigint(20) NOT NULL,
-  `amount` int(11) NOT NULL
+  `amount` int(11) NOT NULL,
+  `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `my_cart`
 --
 
-INSERT INTO `my_cart` (`c_id`, `r_id`, `amount`) VALUES
-('20', 1, 1);
+INSERT INTO `my_cart` (`c_id`, `r_id`, `amount`, `price`) VALUES
+('26', 1, 4, 0);
 
 -- --------------------------------------------------------
 
@@ -208,10 +209,19 @@ CREATE TABLE `recipe` (
   `r_id_region` varchar(60) NOT NULL,
   `r_country` varchar(60) NOT NULL,
   `r_photo` varchar(255) NOT NULL,
-  `recipe_text` varchar(1000) NOT NULL,
+  `recipe_text` text NOT NULL,
   `r_description` text NOT NULL,
-  `r_price` int(11) NOT NULL
+  `r_price` int(11) NOT NULL,
+  `r_time` int(11) NOT NULL,
+  `r_serve` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `recipe`
+--
+
+INSERT INTO `recipe` (`r_id`, `ing_list_id`, `r_name`, `r_type`, `r_time_created`, `r_id_region`, `r_country`, `r_photo`, `recipe_text`, `r_description`, `r_price`, `r_time`, `r_serve`) VALUES
+(1, 1, 'Blue Tarts', 'Pastry', '2018-03-21 19:50:29', '1', 'Canada', '1.jpg', '1. Prepare muffin pans by rolling out pie dough and cutting 4-inch (approx) circles; fit dough circles into muffin cups; set aside in fridge until ready to fill.\r\n2. In a small bowl, place raisins and cover with hot tap water; let stand on the counter for 30 minutes.\r\n3. In a large bowl, using a wooden spoon, mix together the soft butter, brown sugar, salt and corn syrup; stir well until sugar is dissolved and butter is creamed.\r\n4. Add egg and vanilla and mix well.\r\n5. Drain raisins\r\n6. Retrieve tart shells and divide raisins equally into all shells; then divide butter mixture into all tarts\r\n7. Bake at 400F for 15-20 minutes; filling will be lightly browned but still bubbling.\r\n8. Canadians fall into two camps about butter tarts and are quite loyal to their favourite type: runny or firm.\r\n9. If you like firm, bake them for the full 20 minutes, even adding another minute or two if you wish.\r\n10. Let cooked butter tarts cool in pans for 10 minutes after removing from oven; then remove and place on racks until completely cool.\r\n', 'A butter tart is a type of small pastry tart highly regarded in Canadian cuisine and considered one of Canada\'s quintessential treats. It consists of a filling of butter, sugar, syrup, and egg, baked in a pastry shell until the filling is semi-solid with a crunchy top.', 0, 80, 4);
 
 -- --------------------------------------------------------
 
