8d3ffaf87dd2c0d9ea8ad4ea69268cad5fce754b
PrimeroBestia <primebeast@ymail.com>
2018-03-23 01:39:38 +0800

    Profileupdate

--- application/views/UserProfile/addresses.php
+++ application/views/UserProfile/addresses.php
@@ -1,5 +1,4 @@
-
- <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/UserProfile/user.css'); ?>" />
+	 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/UserProfile/user.css'); ?>" />
 	 <link href="https://fonts.googleapis.com/css?family=Spectral:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
      <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
@@ -17,7 +16,7 @@
 				</ul>
 			</div>
 
-			<div class="content">
+			<div class="down-content">
 				<h2>My Addresses</h2>
 				<div class="textalign">
 					<h4>You don't have addresses yet</h4>
@@ -26,6 +25,4 @@
 					</div>
 				</div>
 			</div>
-		</div>
-
 	</div>
