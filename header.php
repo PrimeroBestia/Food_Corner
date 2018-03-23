72f74a5b6a3d16366fd82fc1f19bdb36c9fcdd54
Arjonel Mira Ocampo <34389355+nel014@users.noreply.github.com>
2018-03-23 14:19:50 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,54 @@
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <title>Food Corner</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="<?php echo base_url('assets/FoodC/css/bootstrap.min.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/FoodC/css/carousel.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/FoodC/css/fontAwesome.css') ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/FoodC/css/templatemo-style.css') ?>">
+        <link rel="icon" href="<?php echo base_url('assets/img/cookbook.png') ?>">
+
+    </head>
+
+<body>
+    <div class="header">
+            <nav class="navbar navbar-inverse" role="navigation">
+                <div id="logo-nav">
+                    <img src="<?php echo base_url();?>assets/FoodC/img/logo-nav.png" alt="Logo">
+                </div>
+                <div class="navbar-header">
+                    <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <!--/.navbar-header-->
+                <div id="main-nav" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="<?php echo base_url();?>">Home</a></li>
+                        <li><a href="<?php echo site_url('/RecipeBook');?>">Recipe Book</a></li>
+                        <li><a href="<?php echo site_url('/AboutUsView');?>">About Us</a></li>
+                        <li><a href="<?php echo site_url('/LoginView')?>" >Log in</a></li>
+                        <li><a href="<?php echo site_url('/SignupView')?>" >Sign up</a></li>
+                    </ul>
+                    
+                </div>
+                <!--/.navbar-collapse-->
+                
+            </nav>
+            <!--/.navbar-->
+        </div>
+        <!--/.container-->
+    <!--/.header-->
+    <div>
+    <?php if(isset($_SESSION['alert'])): ?>
+        <?php echo '<p class = "alert alert-success">'.$_SESSION['alert'].'</p>';$_SESSION['alert']=NULL;?>
+    <?php endif;?>
